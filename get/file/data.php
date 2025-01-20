286ddbca0a29c9127fd98d1e0058b29a261c2f20
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-01-20 12:18:29 +0700

    Update data.php

--- get/file/data.php
+++ get/file/data.php
@@ -2,8 +2,8 @@
   #kode as (" ");
  file_get_contents(
    include_path,
-     bool $use_include_path=false,
+     bool $file_use_include_path=false,
    #"halo-dunia/kode/"
-   string $());
+   string $filename());
  echo(file_get_contents());
 ?>
